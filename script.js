9592dd04bd5696ba826882c88f506d1630049383
Natasha Malhan <natashamalhan@natashas-air.lan>
2023-05-25 13:52:35 -0700

    final changes

--- script.js
+++ script.js
@@ -2,25 +2,32 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-$('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY, h:mm'));
-$(".saveBtn").on("click", function(){
-  localStorage.setItem(this.id, $(this).siblings("textarea").val());
-})
-$(".time-block").each(function () {
-  var hour = parseInt($(this).attr("id").split("-")[1]);
-  var currentHour = dayjs().hour();
-  if (hour < currentHour) {
-    $(this).attr("class", "row time-block past");}
-    else if (hour === currentHour) {
-      $(this).attr("class", "row time-block present");}
-      else {
-        $(this).attr("class", "row time-block future");}
-        $(this).sibing("textarea").val(localStorage.getItem(this.id));
+  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY, h:mm"));
+  $(".saveBtn").on("click", function () {
+    var hourKey = $(this).parent().attr("id").split("-")[1];
+    console.log(hourKey);
+    localStorage.setItem(hourKey, $(this).siblings("textarea").val());
+  });
+  $(".time-block").each(function () {
+    var hour = parseInt($(this).attr("id").split("-")[1]);
+    var currentHour = dayjs().hour();
+    if (hour < currentHour) {
+      $(this).attr("class", "row time-block past");
+    } else if (hour === currentHour) {
+      $(this).attr("class", "row time-block present");
+    } else {
+      $(this).attr("class", "row time-block future");
+    }
+  });
 
-})
-
-
-  //
+  //var hourKey = hour.toString(); 
+  for (var i = 9; i < 17; i++) {
+    var hourKey = i.toString();
+    $(`#hour-${hourKey} .description`).val(localStorage.getItem(hourKey))
+  }
+  //$(this).siblings(".description").val(localStorage.getItem(hourKey));
+  
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
